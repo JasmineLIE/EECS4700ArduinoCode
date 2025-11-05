7761475fdb89a9a7d994950149aa70b212a6b081
Lusa <jasminely428@gmail.com>
2025-11-05 15:03:28 -0500

    five buttons

--- UnityController/UnityController.ino
+++ UnityController/UnityController.ino
@@ -1,7 +1,17 @@
-const int Btn_pin = 2;
+const int Btn_pin_3 = 3;
+const int Btn_pin_4 = 4;
+const int Btn_pin_5 = 5;
+const int Btn_pin_6 = 6;
+const int Btn_pin_7 = 7;
+
 
 void setup() {
-  pinMode(Btn_pin, INPUT);
+  pinMode(Btn_pin_3, INPUT);
+  pinMode(Btn_pin_4, INPUT);
+  pinMode(Btn_pin_5, INPUT);
+  pinMode(Btn_pin_6, INPUT);
+  pinMode(Btn_pin_7, INPUT);
+
   Serial.begin(19200); //can use any serial, important that it does not interfere with any other port traffic
   // put your setup code here, to run once:
  
@@ -9,18 +19,37 @@ void setup() {
 }
 
 void loop() {
-  int value = digitalRead(Btn_pin);
+  int pin_3_val = digitalRead(Btn_pin_3);
+    int pin_4_val = digitalRead(Btn_pin_4);
+      int pin_5_val = digitalRead(Btn_pin_5);
+        int pin_6_val = digitalRead(Btn_pin_6);
+          int pin_7_val = digitalRead(Btn_pin_7);
   String data_to_send = "";
 
   //Can adjust this code where it does not send any data at all unless the button is rpessed.
-  if (value > 0) {
-    data_to_send = "1";
-  } else {
-    data_to_send = "0";
-  }
+if (pin_3_val > 0) {
+  data_to_send = "LEFT, " + pin_3_val;
+  Serial.println(data_to_send);
+}
+if (pin_4_val > 0) {
+  data_to_send = "UP, " + pin_4_val;
+  Serial.println(data_to_send);
+}
+if (pin_5_val > 0) {
+  data_to_send = "DOWN, " + pin_5_val;
+  Serial.println(data_to_send);
+}
+if (pin_6_val > 0) {
+  data_to_send = "RIGHT, " + pin_6_val;
+  Serial.println(data_to_send);
+}
+if (pin_7_val > 0) {
+  data_to_send = "ENTER, " + pin_7_val;
+  Serial.println(data_to_send);
+}
  
   delay(10);
-  Serial.println(data_to_send);
+
   Serial.flush();
 
 }
