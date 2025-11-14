ac02deb828c45fe3859ec005cd53d3d88e4c9d89
Lusa <jasminely428@gmail.com>
2025-11-14 15:27:11 -0500

    Update UnityController.ino

--- UnityController/UnityController.ino
+++ UnityController/UnityController.ino
@@ -1,10 +1,11 @@
 
 
-
+const int hall_pin_1 = A0;
+const int hall_pin_2 = A1;
 void setup() {
   
 
-  Serial.begin(9600); //can use any serial, important that it does not interfere with any other port traffic
+  Serial.begin(19200); //can use any serial, important that it does not interfere with any other port traffic
   // put your setup code here, to run once:
  
 
@@ -14,14 +15,14 @@ void loop() {
   
  
  
- int hall_state = analogRead(A0);
+ int hall_state = analogRead(hall_pin_1);
 
- int hall_state_2 = analogRead(A1);
+ int hall_state_2 = analogRead(hall_pin_2);
 
-  Serial.print("Sensor 1: ");
+  Serial.print("A");
   Serial.println(hall_state);
  
-  Serial.print("Sensor 2: ");
+  Serial.print("B");
   Serial.println(hall_state_2);
   delay(1000);
 
