ed65e3b1d31b8a9b6c66a7903b9c30c5782dbd52
Jas <73720805+JasmineLIE@users.noreply.github.com>
2025-11-14 14:08:20 -0500

    Magnet controller support

--- UnityController/UnityController.ino
+++ UnityController/UnityController.ino
@@ -1,52 +1,29 @@
-const int Btn_pin_3 = 3;
-const int Btn_pin_4 = 4;
-const int Btn_pin_5 = 5;
-const int Btn_pin_6 = 6;
-const int Btn_pin_7 = 7;
+
 
 
 void setup() {
-  pinMode(Btn_pin_3, INPUT);
-  pinMode(Btn_pin_4, INPUT);
-  pinMode(Btn_pin_5, INPUT);
-  pinMode(Btn_pin_6, INPUT);
-  pinMode(Btn_pin_7, INPUT);
+  
 
-  Serial.begin(19200); //can use any serial, important that it does not interfere with any other port traffic
+  Serial.begin(9600); //can use any serial, important that it does not interfere with any other port traffic
   // put your setup code here, to run once:
  
 
 }
 
 void loop() {
-  int pin_3_val = digitalRead(Btn_pin_3);
-    int pin_4_val = digitalRead(Btn_pin_4);
-      int pin_5_val = digitalRead(Btn_pin_5);
-        int pin_6_val = digitalRead(Btn_pin_6);
-          int pin_7_val = digitalRead(Btn_pin_7);
-  String data_to_send = "";
-
-  //Can adjust this code where it does not send any data at all unless the button is rpessed.
-if (pin_3_val == HIGH) {
-  data_to_send = "LEFT";
-
-} else if (pin_4_val== HIGH ) {
-  data_to_send = "UP";
+  
  
-} else if (pin_5_val == HIGH) {
-  data_to_send = "DOWN";
-
-} else if (pin_6_val == HIGH) {
-  data_to_send = "RIGHT";
-
-} else if (pin_7_val == HIGH) {
-  data_to_send = "ENTER";
-
-}
-
-  Serial.println(data_to_send);
  
-  delay(100);
+ int hall_state = analogRead(A0);
+
+ int hall_state_2 = analogRead(A1);
+
+  Serial.print("Sensor 1: ");
+  Serial.println(hall_state);
+ 
+  Serial.print("Snesor 2: ");
+  Serial.println(hall_state_2);
+  delay(1000);
 
   Serial.flush();
 
