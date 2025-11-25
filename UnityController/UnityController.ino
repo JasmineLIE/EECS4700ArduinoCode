4d097246000952bacf24850a74bb10f6099795e7
Jas <73720805+JasmineLIE@users.noreply.github.com>
2025-11-25 10:28:17 -0500

    Analog implementation

--- UnityController/UnityController.ino
+++ UnityController/UnityController.ino
@@ -1,35 +1,32 @@
 
 
-const int hall_pin_1 = 1;
-const int hall_pin_2 = 2;
-const int threshold = 10;
+const int hall_pin_1 = A0;
+const int hall_pin_2 = A1;
+
 void setup() {
   
 
-  Serial.begin(19200); //can use any serial, important that it does not interfere with any other port traffic
+  Serial.begin(9600); //can use any serial, important that it does not interfere with any other port traffic
   // put your setup code here, to run once:
  
-  pinMode(hall_pin_1, INPUT);
-  pinMode(hall_pin_2, INPUT);
 }
 
 void loop() {
   
  
  
- int hall_state_1 = digitalRead(hall_pin_1);
+ int hall_state_1 = analogRead(hall_pin_1);
 
- int hall_state_2 = digitalRead(hall_pin_2);
+ int hall_state_2 = analogRead(hall_pin_2);
 
- if (hall_state < threshold) {
-  Serial.print("A:" + hall_state_!);
-  Serial.println(hall_state);
+
+  Serial.print("A");
+  Serial.println(hall_state_1);
  
- }
-if (hall_state_2 < threshold) {
+
  Serial.print("B");
   Serial.println(hall_state_2);
-}
+
 
  
   delay(1000);
